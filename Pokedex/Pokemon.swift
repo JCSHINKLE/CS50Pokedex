38010056313c3415ee7ee04c463f852af7eacda8
Joshua Shinkle <joshuashinkle@NaPali.local>
2020-04-30 17:38:25 -0400

    Pokedex Done

--- Pokedex/Pokemon.swift
+++ Pokedex/Pokemon.swift
@@ -28,3 +28,16 @@ struct PokemonType: Codable {
 struct PokemonSprites: Codable {
     let front_default: String
 }
+
+struct PokemonDescription: Codable {
+    let flavor_text_entries: [PokemonFlavor]
+}
+
+struct PokemonFlavor: Codable {
+    let flavor_text: String
+    let language: PokemonLanguage
+}
+
+struct PokemonLanguage: Codable {
+    let name: String
+}
--- Pokedex/PokemonViewController.swift
+++ Pokedex/PokemonViewController.swift
@@ -1,7 +1,5 @@
 import UIKit
 
-//var caughtPokemonDict = [String: Bool]()
-
 class PokemonViewController: UIViewController {
     var url: String!
     
@@ -11,17 +9,17 @@ class PokemonViewController: UIViewController {
     @IBOutlet var type2Label: UILabel!
     @IBOutlet var catchButton: UIButton!
     @IBOutlet var spriteImage: UIImageView!
+    @IBOutlet var descriptionText: UITextView!
+
     
     @IBAction func toggleCatch() {
         if UserDefaults.standard.bool(forKey: self.nameLabel.text!) == true {
             UserDefaults.standard.set(false, forKey: self.nameLabel.text!)
             catchButton.setTitle("Catch", for: [])
-            //print("Toggle False = \(UserDefaults.standard.bool(forKey: self.nameLabel.text!))")
         }
         else {
             UserDefaults.standard.set(true, forKey: self.nameLabel.text!)
             catchButton.setTitle("Release", for: [])
-            //print("Toggle True = \(UserDefaults.standard.bool(forKey: self.nameLabel.text!))")
         }
     }
     
@@ -37,21 +35,20 @@ class PokemonViewController: UIViewController {
         type1Label.text = ""
         type2Label.text = ""
         catchButton.setTitle("", for: [])
-        //print("Before Load = \(UserDefaults.standard.bool(forKey: self.nameLabel.text!))")
+        descriptionText.text = ""
+        
         loadPokemon()
     }
-
+ 
     func loadPokemon() {
         URLSession.shared.dataTask(with: URL(string: url)!) { (data, response, error) in
             guard let data = data else {
                 return
             }
-
             do {
                 let result = try JSONDecoder().decode(PokemonResult.self, from: data)
                 DispatchQueue.main.async {
                     self.navigationItem.title = self.capitalize(text: result.name)
-                    
                     do {
                         self.spriteImage.image = UIImage(data: try Data(contentsOf: URL(string: result.sprites.front_default)!))
                     }
@@ -69,12 +66,36 @@ class PokemonViewController: UIViewController {
                             self.type2Label.text = typeEntry.type.name
                         }
                     }
-                    //print("Loading = \(UserDefaults.standard.bool(forKey: self.nameLabel.text!))")
+
                     if UserDefaults.standard.bool(forKey: self.nameLabel.text!) == true {
                         self.catchButton.setTitle("Release", for: [])
                     }
-                    else if UserDefaults.standard.bool(forKey: self.nameLabel.text!) == false /*|| UserDefaults.standard.bool(forKey: self.nameLabel.text!) == nil*/ {
+                    else if UserDefaults.standard.bool(forKey: self.nameLabel.text!) == false {
                         self.catchButton.setTitle("Catch", for: [])
+                    }
+                    self.loadDescription(id: result.id)
+                }
+            }
+            catch let error {
+                print(error)
+            }
+        }.resume()
+    }
+    
+    func loadDescription(id: Int) {
+        URLSession.shared.dataTask(with: URL(string: "https://pokeapi.co/api/v2/pokemon-species/\(id)")!) { (data, response, error) in
+            guard let data = data else {
+                return
+            }
+            do {
+                let description = try JSONDecoder().decode(PokemonDescription.self, from: data)
+                var first = true
+                DispatchQueue.main.async {
+                    for flavor in description.flavor_text_entries {
+                        if flavor.language.name == "en" && first == true {
+                            self.descriptionText.text = flavor.flavor_text
+                            first = false
+                        }
                     }
                 }
             }
